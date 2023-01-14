631bf09af229b0c4f2978bf5823ac88969fc4191
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-01-13 21:10:43 -0500

    update login styles

---
+++ src/js/virtual.js
@@ -0,0 +1,7 @@
+import handleLoader from "./helpers/loader.js";
+
+const d = document,
+	$modalLoader = d.querySelector(".modal-loader");
+d.addEventListener("DOMContentLoaded", (e) => {
+	handleLoader($modalLoader);
+});
