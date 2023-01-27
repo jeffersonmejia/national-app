aa05c185be1d4781830940f17deafaa2f25dbbdc
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-01-26 19:57:44 -0500

    add callout animation

--- public/js/index.js
+++ public/js/index.js
@@ -1,2 +1,10 @@
 import loadConfig from "./helpers/config.js";
 loadConfig();
+
+const d = document,
+	$btnCallout = d.querySelector("header .btn");
+d.addEventListener("DOMContentLoaded", (e) => {
+	setTimeout(() => {
+		$btnCallout.classList.add("visible");
+	}, 2200);
+});
