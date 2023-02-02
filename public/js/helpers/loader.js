55be17b80e2b30a68dae0c7331f177ff95ad2c3c
Jefferson Mejia <jeffersonmejiach01@gmail.com>
2023-02-01 20:01:40 -0500

    add register form connection and successful messages

--- public/js/helpers/loader.js
+++ public/js/helpers/loader.js
@@ -3,3 +3,11 @@ export default function handleLoader(loader) {
 		loader.classList.add("loader-bar-disabled");
 	}, 1500);
 }
+
+export function setLoaderStatus(loader, status) {
+	if (status) {
+		loader.classList.remove("loader-bar-disabled");
+	} else {
+		loader.classList.add("loader-bar-disabled");
+	}
+}
